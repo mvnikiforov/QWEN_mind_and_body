3d5093daf0ab36a97e63061ea559c195474d118b
Max <mv.nikiforov@gmail.com>
2026-09-16 11:39:41 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/QWEN_mind_and_body/",
+  base: "/", //"/QWEN_mind_and_body/",
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
